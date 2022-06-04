12a89a330738b6bd9bc7d019de08a64823c4d7bb
jhsseonn <hosu0125@naver.com>
2022-06-05 02:15:10 +0900

    6월 첫째주 3

---
+++ 6월 첫째주/2805.py
@@ -0,0 +1,20 @@
+# 백준 알고리즘 2805번
+# 나무 자르기
+
+n, m = map(int, input().split())
+tree = list(map(int, input().split()))
+start, end = 1, max(tree)
+
+while start<=end:
+    mid = (start+end)//2
+
+    log = 0
+    for i in tree:
+        if i>=mid:
+            log+=i-mid
+    
+    if log>=m:
+        start=mid+1
+    else:
+        end=mid-1
+print(end)
