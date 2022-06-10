c4d4a7682ce782ad9d968f90223bec9a3d805ce3
jhsseonn <hosu0125@naver.com>
2022-06-11 00:39:57 +0900

    6월 둘째주 1

---
+++ 6월 둘째주/11052.py
@@ -0,0 +1,11 @@
+#백준 알고리즘 11052번
+#카드 구매하기
+
+n = int(input())
+p = [0]+list(map(int, input().split()))
+dp = [0 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for k in range(1, i+1):
+        dp[i]=max(dp[i], dp[i-k]+p[k])
+print(dp[i])
